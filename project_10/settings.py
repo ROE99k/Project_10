65733db499a5d067703326ed764203fa96489af6
ROE99k <roneecarumba99@gmail.com>
2025-06-03 11:22:06 +0545

    allowd host

--- project_10/settings.py
+++ project_10/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY", default="defaultkey123")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
